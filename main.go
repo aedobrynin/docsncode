53bdd37e99411219eeb430a676fa324417aab7d9
aedobrynin <hashlib@yandex.ru>
2025-05-06 21:10:16 +0300

    Fixed flags

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/urfave/cli/v3"
 
 	"docsncode/buildcache"
 )
@@ -38,61 +41,92 @@ func initBuildCache(forceRebuild, noCache bool, absPathToProjectRoot, absPathToR
 func main() {
 	log.SetOutput(os.Stderr)
 
-	// TOFIX: флаги не считываются
-	forceRebuild := flag.Bool("force-rebuild", false, "ignore cached values and rebuild whole result")
-	noCache := flag.Bool("no-cache", false, "ignore cached values and do not cache the result")
+	cmd := &cli.Command{
+		Name:  "docsncode",
+		Usage: "An application to unite code and documentation",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "force-rebuild",
+				Usage: "Ignore cached result and build new result",
+			},
+			&cli.BoolFlag{
+				Name:  "no-cache",
+				Usage: "Ignore cached result and do not cache new result",
+			},
+		},
+		UsageText: "docsncode <path-to-project-root> <path-to-result-dir> [path-to-cache-file] [--force-rebuild] [--no-cache]",
+		Action: func(_ context.Context, c *cli.Command) error {
+			if c.Args().Len() < 1 {
+				log.Fatal("path-to-project-root is not provided")
+			}
+			if c.Args().Len() < 2 {
+				log.Fatal("path-to-result-dir is not provided")
+			}
+			if c.Args().Len() > 3 {
+				log.Fatal("Too many positional args")
+			}
+			pathToProjectRoot := c.Args().Get(0)
+			pathToResultDir := c.Args().Get(1)
+			pathToCacheFile := c.Args().Get(2)
+			if pathToCacheFile == "" {
+				pathToCacheFile = filepath.Join(pathToProjectRoot, ".docsncode_cache.json")
+			}
+			forceRebuild := c.Bool("force-rebuild")
+			noCache := c.Bool("no-cache")
 
-	flag.Parse()
+			fmt.Println("path-to-project-root:", pathToProjectRoot)
+			fmt.Println("path-to-result-dir:", pathToResultDir)
+			fmt.Println("path-to-cache-file:", pathToCacheFile)
+			fmt.Println("force-rebuild:", forceRebuild)
+			fmt.Println("no-cache:", noCache)
 
-	if flag.NArg() < 2 {
-		log.Fatalf("Usage: docsncode <path-to-project-root> <path-to-result-dir> [path-to-cache-file] [--force-rebuild] [--no-cache]")
+			log.Printf("path_to_project_root=%s, path_to_result_dir=%s, path_to_cache_file=%s, force_rebuild=%t, no_cache=%t", pathToProjectRoot, pathToResultDir, pathToCacheFile, forceRebuild, noCache)
+
+			// TODO: правда ли, что это должно происходить тут?
+			err := os.MkdirAll(pathToResultDir, 0755)
+			if err != nil {
+				log.Fatalf("error on creating result directory: %v", err)
+			}
+
+			absPathToProjectRoot, err := filepath.Abs(pathToProjectRoot)
+			if err != nil {
+				log.Fatalf("error on getting abs path to project root: %v", err)
+			}
+
+			absPathToResultDir, err := filepath.Abs(pathToResultDir)
+			if err != nil {
+				log.Fatalf("error on getting abs path to result dir: %v", err)
+			}
+
+			absPathToCacheDataFile, err := filepath.Abs(pathToCacheFile)
+			if err != nil {
+				log.Fatalf("error on getting abs path to cache data file: %v", err)
+			}
+
+			buildCache := initBuildCache(forceRebuild, noCache, absPathToProjectRoot, absPathToResultDir, absPathToCacheDataFile)
+
+			// @docsncode
+			// Here we use function from [html.go](html/html.go)
+			// @docsncode
+
+			err = buildDocsncode(pathToProjectRoot, pathToResultDir, buildCache)
+			if err != nil {
+				log.Fatalf("error on building docsncode: %v", err)
+			}
+			log.Printf("written result to %s", pathToResultDir)
+			// TODO: не должны ли мы дампить кэш при ошибке?
+			err = buildCache.Dump()
+			if err != nil {
+				log.Printf("error on dumping build cache: %v", err)
+			}
+
+			return nil
+		},
 	}
-	pathToProjectRoot := flag.Arg(0)
-	pathToResultDir := flag.Arg(1)
-	var pathToCacheFile string
-	if flag.NArg() > 2 {
-		pathToCacheFile = flag.Arg(2)
-	} else {
-		pathToCacheFile = filepath.Join(pathToProjectRoot, ".docsncode_cache.json")
-	}
 
-	log.Printf("path_to_project_root=%s, path_to_result_dir=%s, path_to_cache_file=%s, force_rebuild=%t, no_cache=%t", pathToProjectRoot, pathToResultDir, pathToCacheFile, *forceRebuild, *noCache)
-
-	// TODO: правда ли, что это должно происходить тут?
-	err := os.MkdirAll(pathToResultDir, 0755)
+	err := cmd.Run(context.Background(), os.Args)
 	if err != nil {
-		log.Fatalf("error on creating result directory: %v", err)
-	}
-
-	absPathToProjectRoot, err := filepath.Abs(pathToProjectRoot)
-	if err != nil {
-		log.Fatalf("error on getting abs path to project root: %v", err)
-	}
-
-	absPathToResultDir, err := filepath.Abs(pathToResultDir)
-	if err != nil {
-		log.Fatalf("error on getting abs path to result dir: %v", err)
-	}
-
-	absPathToCacheDataFile, err := filepath.Abs(pathToCacheFile)
-	if err != nil {
-		log.Fatalf("error on getting abs path to cache data file: %v", err)
-	}
-
-	buildCache := initBuildCache(*forceRebuild, *noCache, absPathToProjectRoot, absPathToResultDir, absPathToCacheDataFile)
-
-	// @docsncode
-	// Here we use function from [html.go](html/html.go)
-	// @docsncode
-	err = buildDocsncode(pathToProjectRoot, pathToResultDir, buildCache)
-	if err != nil {
-		log.Fatalf("error on building docsncode: %v", err)
-	}
-	log.Printf("written result to %s", pathToResultDir)
-	// TODO: не должны ли мы дампить кэш при ошибке?
-	err = buildCache.Dump()
-	if err != nil {
-		log.Printf("error on dumping build cache: %v", err)
+		log.Fatal(err)
 	}
 
 	/* @docsncode
